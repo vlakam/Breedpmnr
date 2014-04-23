d6ce440ccd0f34b6d17990b5f089a3c8a8ae585c
ATrigger <alextrigger@2ch.hk>
2014-04-23 18:34:36 +0400

    License fixed. Site removed in a side repositary

--- www/application/views/header.php
+++
@@ -1,8 +0,0 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Breed screengrab">
-<meta name="Keywords" content="Breed, PMNR, BreedPMNR, скрин, screen"> 
-<meta name="author" content="Trigger">
-<title><?=$title?></title>
-<link href="<?=base_url("application/public");?>/css/bootstrap.css" rel="stylesheet">
-<link href="<?=base_url("application/public");?>/css/mycss.css" rel="stylesheet">
